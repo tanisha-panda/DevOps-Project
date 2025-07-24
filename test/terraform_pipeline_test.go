a8bafa871c59f60cafb8f2a8bd759a3280644f68
Tanisha Panda <120770711+tanisha-panda@users.noreply.github.com>
2025-07-25 02:11:08 +0530

    Update terraform_pipeline_test.go

--- test/terraform_pipeline_test.go
+++ test/terraform_pipeline_test.go
@@ -17,6 +17,6 @@ func TestTerraformPipeline(t *testing.T) {
   defer terraform.Destroy(t, tf)             // Cleanup resources after test
   terraform.InitAndApply(t, tf)             // Run terraform init + apply
 
-  pipelineName := terraform.Output(t, tf, "codepipeline_devops") // Replace with actual output variable name
+  pipelineName := terraform.Output(t, tf, "codepipeline_name") // Replace with actual output variable name
   assert.NotEmpty(t, pipelineName)
 }
