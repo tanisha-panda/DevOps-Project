5d4c11683524e30a10e8d7722bb5a0fd60c43e39
Tanisha Panda <120770711+tanisha-panda@users.noreply.github.com>
2025-07-24 19:06:35 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+}
+
 variable "bucket_name" {
   description = "Unique S3 bucket name for storing artifacts"
   type        = string
@@ -14,21 +24,48 @@ variable "instance_type" {
 }
 
 variable "key_name" {
-  description = "SSH key name to access EC2"
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repo containing app code"
+  type        = string
+}
+
+variable "github_branch" {
+  description = "GitHub branch to track"
   type        = string
 }
 
 variable "github_token" {
-  description = "GitHub Personal Access Token for CodePipeline"
+  description = "GitHub Personal Access Token (unused in Terraform but may be required in CI/CD)"
+  type        = string
+}
+
+variable "codestar_connection_arn" {
+  description = "ARN of the CodeStar GitHub connection"
   type        = string
 }
 
 variable "instance_tag_key" {
-  type    = string
-  default = "Name"
+  description = "Key used for tagging EC2 instance"
+  type        = string
+  default     = "Name"
 }
 
 variable "instance_tag_value" {
-  type    = string
-  default = "MyAppServer"
+  description = "Value used for tagging EC2 instance"
+  type        = string
+  default     = "MyAppServer"
+}
+
+variable "codebuild_image" {
+  description = "Docker image for CodeBuild environment"
+  default     = "aws/codebuild/standard:6.0"
 }
