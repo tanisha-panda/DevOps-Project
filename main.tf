e6c164f0f41a8ea74b477736c99b054237ceaa39
Tanisha Panda <120770711+tanisha-panda@users.noreply.github.com>
2025-07-26 19:49:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -169,9 +169,9 @@ resource "aws_codebuild_project" "build" {
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
-    type                        = "LINUX_CONTAINER"
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    type         = "LINUX_CONTAINER"
   }
 
   source {
