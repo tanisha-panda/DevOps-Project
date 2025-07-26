b3d83566e832287b997d37def9736ec58f23d601
Tanisha Panda <120770711+tanisha-panda@users.noreply.github.com>
2025-07-26 14:40:53 +0530

    Add files via upload

--- test/terraform_pipeline_test.go
+++ test/terraform_pipeline_test.go
@@ -1,26 +1,22 @@
 package test
 
 import (
-	"testing"
+  "testing"
 
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
+  "github.com/gruntwork-io/terratest/modules/terraform"
+  "github.com/stretchr/testify/assert"
 )
 
 func TestTerraformPipeline(t *testing.T) {
-	t.Parallel()
+  t.Parallel()
 
-	tf := &terraform.Options{
-		TerraformDir: "../terraform", // Adjust this path if your Terraform code is in a different directory
-		Vars: map[string]interface{}{
-			"codepipeline_name":       "my-devops-project-pipeline",
-			"codestar_connection_arn": "arn:aws:codeconnections:eu-north-1:932117065109:connection/53a86147-18c6-4a50-b31e-4b3aecd20069",
-		},
-	}
+  tf := &terraform.Options{
+    TerraformDir: "../terraform", // Path to your Terraform config
+  }
 
-	defer terraform.Destroy(t, tf) // Clean up resources after test
-	terraform.InitAndApply(t, tf) // Initialize and apply Terraform
+  defer terraform.Destroy(t, tf)             // Cleanup resources after test
+  terraform.InitAndApply(t, tf)             // Run terraform init + apply
 
-	pipelineName := terraform.Output(t, tf, "codepipeline_name")
-	assert.Equal(t, "my-devops-project-pipeline", pipelineName)
+  pipelineName := terraform.Output(t, tf, "codepipeline_name") // Updated to match output.tf
+  assert.NotEmpty(t, pipelineName)
 }
